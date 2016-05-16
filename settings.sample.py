50fa947339eb2123ff9c074ddd4b9651a22442f8
teashaw <xiaopeiqing@gmail.com>
2016-05-16 07:47:14 +0000

    changed settings.sample.py

--- settings.sample.py
+++ settings.sample.py
@@ -1,7 +1,8 @@
-SECRET_KEY = '\x10\x1a^\xdf\xb8R}z\xce\xb4\x83\xb9b\x13\xd3]\x9a\x11\x12\x17\xb6\x05\xaf\xdb'
+#This is a sample file ,please change the name to setting.py in production env.
+SECRET_KEY ='xxx' 
 DEBUG = True
-DB_USERNAME = 'teaguexiao'
-DB_PASSWORD = ''
-BLOG_DATABASE_NAME = 'blog'
+DB_USERNAME = 'sample'
+DB_PASSWORD = 'sample'
+BLOG_DATABASE_NAME = 'sample'
 SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@localhost:3306/%s' % (DB_USERNAME, DB_PASSWORD,BLOG_DATABASE_NAME)
 
