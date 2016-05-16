d9f09feefd9cd0f71e3158b48d8a6cafc8bf7989
teashaw <xiaopeiqing@gmail.com>
2016-05-16 07:06:15 +0000

    remove cache of settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = '\x10\x1a^\xdf\xb8R}z\xce\xb4\x83\xb9b\x13\xd3]\x9a\x11\x12\x17\xb6\x05\xaf\xdb'
-DEBUG = True
-DB_USERNAME = 'teaguexiao'
-DB_PASSWORD = ''
-BLOG_DATABASE_NAME = 'blog'
-SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@localhost:3306/%s' % (DB_USERNAME, DB_PASSWORD,BLOG_DATABASE_NAME)
-
